57047c3f4fa1ff9fc36e1f9bc3e33e5f0174c1d2
Federico <isma-fb@hotmail.com>
2023-01-25 13:53:04 +0100

    .env preparado para despliegue de app

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -14,7 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
